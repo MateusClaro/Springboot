988abac19f5fdbf1537b6e37ee785a1622473dae
Mateus Filipe Claro <74942467+MateusClaro@users.noreply.github.com>
2020-12-03 13:49:03 -0300

    Add files via upload

---
+++ src/main/java/com/example/demo/api/CarrosController.java
@@ -0,0 +1,27 @@
+package com.example.demo.api;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.carros.domain.Carro;
+import com.example.carros.domain.CarroService;
+
+@RestController
+@RequestMapping("/api/v1/carros")
+
+public class CarrosController {
+	@Autowired
+	private CarroService service;
+	
+	@GetMapping
+	public List<Carro> get() {
+		return service.getCarros(); 
+	}
+	
+
+}
